9cc69859e6f1d6f8120afb64e8d51d3388064197
khai96_ <hvksmr1996@gmail.com>
2016-07-04 10:56:35 +0700

    complete fundamental changes

--- index.js
+++ index.js
@@ -1,57 +1,245 @@
 
-(module => {
-    'use strict';
+'use strict'
 
-    var {
-        getOwnPropertyNames,
-        getPrototypeOf,
-        create: createObject
-    } = Object
+var XIterable = require('x-iterable-base/template')
+var pair = require('./pair.js')
 
-    class ObjectMap {
+var {
+    Pair,
+    ConfiguredPropertyIterator,
+    DataPropertyIterator,
+    AccessorPropertyIterator
+} = pair
 
-        constructor(object) {
-            return {
-                object,
-                properties: getOwnPropertyNames(object),
-                __proto__: this
-            }
+var {
+    getOwnPropertyNames,
+    getOwnPropertySymbols,
+    getOwnPropertyDescriptor,
+    defineProperty,
+    getPrototypeOf,
+    create: createObject
+} = Object
+
+var {
+    iterator
+} = Symbol
+
+var objectKeyIteratorCreators = {
+
+    ownnames: object =>
+        () => getOwnPropertyNames(object)[iterator](),
+
+    ownsymbols: object =>
+        () => getOwnPropertySymbols(object)[iterator](),
+
+    ownkeys: object => function * () {
+        yield * getOwnPropertyNames(object)
+        yield * getOwnPropertySymbols(object)
+    },
+
+    __proto__: null
+
+}
+
+var mksibling = object =>
+    createObject(getPrototypeOf(object))
+
+var mkownmap = (map, self) => ({map, __proto__: self})
+
+var Root = XIterable(class {
+
+    constructor(object, type = 'ownkeys') {
+        var iterate = objectKeyIteratorCreators[type];
+        return {
+            object, type,
+            getKeys: iterate(object),
+            __proto__: this
         }
+    }
 
-        mapDataProperties(fn) {
-            var {object, properties} = this;
-            var result = createObject(getPrototypeOf(object));
-            for (let property of properties) {
-                let {name, value} = fn({name: property, value: object[property]}, this);
-                result[name] = value;
-            }
-            return new this.constructor(result);
-        }
+    get configures() {
+        return new ConfiguredPropertyIterable(this.object)
+    }
 
-        mapDataPropertyNames(fn) {
-            return this.mapDataProperties(
-                ({name, value}) => ({name: fn(name, this), value})
-            );
-        }
+    get data() {
+        return new DataPropertyIterable(this.object)
+    }
 
-        mapDataPropertyValues(fn) {
-            return this.mapDataProperties(
-                ({name, value}) => ({name, value: fn(value, this)})
-            );
-        }
+    get accessors() {
+        return new AccessorPropertyIterable(this.object)
+    }
 
-        swapDataPropertyValues() {
-            return this.mapDataProperties(
-                ({name, value}) => ({name: value, value: name})
-            );
-        }
+    getObject() {
+        return this.object
+    }
 
-        getObject() {
-            return this.object;
+})
+
+class ConfiguredPropertyIterable extends Root {
+
+    constructor(object, type) {
+
+        var {getKeys} = super(object, type)
+
+        return {
+
+            * [iterator]() {
+                for (let key of getKeys()) {
+                    yield new ConfiguredPropertyIterator(key, getOwnPropertyDescriptor(object, key))
+                }
+            },
+
+            get object() {
+                var result = mksibling(object)
+                for (let [key, descriptor] of this) {
+                    defineProperty(result, key, descriptor)
+                }
+                return result
+            },
+
+            get keys() {
+                return mkownmap(
+                    fn => this.map(
+                        ([key, descriptor]) =>
+                            new ConfiguredPropertyIterator(fn(key), descriptor)
+                    ),
+                    this
+                )
+            },
+
+            get descriptors() {
+                return mkownmap(
+                    fn => this.map(
+                        ([key, descriptor]) =>
+                            new ConfiguredPropertyIterator(key, fn(descriptor))
+                    ),
+                    this
+                )
+            },
+
+            __proto__: this
+
         }
 
     }
 
-    module.exports = class extends ObjectMap {};
+}
 
-})(module);
+class DataPropertyIterable extends Root {
+
+    constructor(object, type) {
+
+        var {getKeys} = super(object, type)
+
+        return {
+
+            * [iterator]() {
+                for (let key of getKeys()) {
+                    yield new DataPropertyIterator(key, object[key])
+                }
+            },
+
+            get object() {
+                var result = mksibling(object)
+                for (let [key, value] of this) {
+                    result[key] = value
+                }
+                return result
+            },
+
+            get keys() {
+                return mkownmap(
+                    fn => this.map(
+                        ([key, value]) =>
+                            new DataPropertyIterator(fn(key), value)
+                    ),
+                    this
+                )
+            },
+
+            get values() {
+                return mkownmap(
+                    fn => this.map(
+                        ([key, value]) =>
+                            new DataPropertyIterator(key, fn(value))
+                    ),
+                    this
+                )
+            },
+
+            __proto__: this
+
+        }
+
+    }
+
+}
+
+class AccessorPropertyIterable extends Root {
+
+    constructor(object, type) {
+
+        var {getKeys} = super(object, type)
+
+        return {
+
+            * [iterator]() {
+                for (let [key, {get, set, value}] of new ConfiguredPropertyIterable(object)) {
+                    yield new AccessorPropertyIterator(key, get || (() => value), set || (x => value = x))
+                }
+            },
+
+            get object() {
+                var result = mksibling(object)
+                for (let [key, get, set] of this) {
+                    defineProperty(result, key, {get, set, enumerable: true})
+                }
+                return result
+            },
+
+            get keys() {
+                return mkownmap(
+                    fn => this.map(
+                        ([key, get, set]) =>
+                            new AccessorPropertyIterator(fn(key), get, set)
+                    ),
+                    this
+                )
+            },
+
+            get getters() {
+                return mkownmap(
+                    fn => this.map(
+                        ([key, get, set]) =>
+                            new AccessorPropertyIterator(key, fn(get), set)
+                    ),
+                    this
+                )
+            },
+
+            get setters() {
+                return mkownmap(
+                    fn => this.map(
+                        ([key, get, set]) =>
+                            new AccessorPropertyIterator(key, get, fn(set))
+                    ),
+                    this
+                )
+            },
+
+            __proto__: this
+
+        }
+
+    }
+
+}
+
+module.exports = {
+    Root,
+    ConfiguredPropertyIterable,
+    DataPropertyIterable,
+    AccessorPropertyIterable,
+    iterate: (...args) => new Root(...args),
+    __proto__: pair
+}
---
+++ pair.js
@@ -0,0 +1,36 @@
+
+'use strict'
+
+class Pair extends Array {
+    get key() {
+        return this[0]
+    }
+}
+
+class ConfiguredPropertyIterator extends Pair {
+    get descriptor() {
+        return this[1]
+    }
+}
+
+class DataPropertyIterator extends Pair {
+    get value() {
+        return this[1]
+    }
+}
+
+class AccessorPropertyIterator extends Pair {
+    get get() {
+        return this[1]
+    }
+    get set() {
+        return this[2]
+    }
+}
+
+module.exports = {
+    Pair,
+    ConfiguredPropertyIterator,
+    DataPropertyIterator,
+    AccessorPropertyIterator
+}
